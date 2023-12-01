3f6ccfc25dc01aa14331d76a705542af4163ad8c
rasiksilwal013 <rasiksilwal@gmail.com>
2023-11-30 20:15:33 -0500

    eraser size adjusted

--- index.js
+++ index.js
@@ -1,38 +1,106 @@
-// index.js
-const canvas = document.getElementById("main"),
-      ctx = canvas.getContext("2d");
+document.addEventListener("DOMContentLoaded", function () {
+    // Canvas and context
+    var canvas = document.getElementById("main");
+    var ctx = canvas.getContext("2d");
 
-let isDrawing = false,
-    brushWidth = 5,
-    currentColor = 'black';
+    // Drawing variables
+    var painting = false;
+    var brushColor = "#000000"; // Initial color is black
+    var brushSize = 5;
 
-window.addEventListener("load", () => {
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+    // Event listeners
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", endPosition);
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mouseout", endPosition);
+
+    // Button listeners
+    document.getElementById("new").addEventListener("click", function () {
+        clearCanvas();
+    });
+
+    document.getElementById("erase").addEventListener("click", function () {
+        brushColor = "white";
+        setCursor("url('data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"12\" height=\"12\" viewBox=\"0 0 12 12\"><circle cx=\"5\" cy=\"5\" r=\"4\" fill=\"white\" border=\"black\"/></svg>') 5 5, auto");
+    });
+
+    document.getElementById("black").addEventListener("click", function () {
+        brushColor = "#000000";
+        setCursor("auto");
+    });
+
+    document.getElementById("pink").addEventListener("click", function () {
+        brushColor = "#F50057";
+        setCursor("auto");
+    });
+
+    document.getElementById("blue").addEventListener("click", function () {
+        brushColor = "#2979FF";
+        setCursor("auto");
+    });
+
+    document.getElementById("yellow").addEventListener("click", function () {
+        brushColor = "#FFD600";
+        setCursor("auto");
+    });
+
+    // Slider listener
+    var slider = document.getElementById("slider");
+    slider.addEventListener("input", function () {
+        brushSize = this.value;
+        updateBrushSizeDisplay();
+        setCursorStyle(brushSize)
+    });
+
+    // Function to update brush size display
+    function updateBrushSizeDisplay() {
+        document.getElementById("brushSize").textContent = brushSize;
+    }
+
+    // Function to set the cursor style
+    function setCursor(cursorStyle) {
+        canvas.style.cursor = cursorStyle;
+    }
+
+    // Function to set the cursor style based on brush size
+    function setCursorStyle(size) {
+        var cursorURL = `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" viewBox="0 0 ${size} ${size}"><circle cx="${size / 2}" cy="${size / 2}" r="${size / 2 - 1}" fill="white" stroke="black" stroke-width="1"/></svg>') ${size / 2} ${size / 2}, auto`;
+        setCursor(cursorURL);
+    }
+
+    // Function to start drawing
+    function startPosition(e) {
+        painting = true;
+        draw(e);
+    }
+
+    // Function to stop drawing
+    function endPosition() {
+        painting = false;
+        ctx.beginPath(); // Start a new path for future drawings
+    }
+
+    // Function to draw on the canvas
+    function draw(e) {
+        if (!painting) return;
+
+        ctx.lineWidth = brushSize;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = brushColor;
+
+        // Calculate the correct mouse position relative to the canvas
+        var rect = canvas.getBoundingClientRect();
+        var mouseX = e.clientX - rect.left;
+        var mouseY = e.clientY - rect.top;
+
+        ctx.lineTo(mouseX, mouseY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(mouseX, mouseY);
+    }
+
+    // Function to clear the canvas
+    function clearCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
 });
-
-const startDraw = () => {
-    isDrawing = true;
-    ctx.beginPath();
-    ctx.lineWidth = brushWidth;
-    ctx.strokeStyle = currentColor;
-};
-
-const drawing = (e) => {
-    if (!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-};
-
-canvas.addEventListener("mousedown", startDraw);
-canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", () => isDrawing = false);
-
-const changeColor = (color) => {
-    currentColor = color;
-};
-
-const changeBrushSize = () => {
-    brushWidth = document.getElementById("slider").value;
-    document.getElementById("brushSize").innerText = brushWidth;
-};
